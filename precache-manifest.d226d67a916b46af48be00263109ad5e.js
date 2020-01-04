7dba0f0deca20315916bb6f4c8bae8dff2837ff7
josue rojas <josuerojas.rojas@gmail.com>
2020-01-03 21:04:40 -0600

    Updates

---
+++ precache-manifest.d226d67a916b46af48be00263109ad5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc59fcdf8ad2c4aeead791efebce0f3",
+    "url": "/dots/index.html"
+  },
+  {
+    "revision": "edf4cba36d4e42e67b13",
+    "url": "/dots/static/css/main.68953ebb.chunk.css"
+  },
+  {
+    "revision": "aa665594e088b85035b8",
+    "url": "/dots/static/js/2.932e43d8.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/dots/static/js/2.932e43d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edf4cba36d4e42e67b13",
+    "url": "/dots/static/js/main.7a7a8315.chunk.js"
+  },
+  {
+    "revision": "d5bdcea48766ac68f444",
+    "url": "/dots/static/js/runtime-main.9e072669.js"
+  }
+]);
